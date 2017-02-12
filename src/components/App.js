359353e4202bd04223ef835672808a139e073966
GiTo RoyBoy <GiTo RoyBoy>
2017-02-12 18:54:39 +0100

    setting up the env

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+//THis component handles the app template used on every page.
+import React, {PropTypes} from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <p>Header Here....</p>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+App.PropTypes = {
+    children: PropTypes.object.isRequired
+};
+
+export default App;
