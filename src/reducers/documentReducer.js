72cf14a45fa6d107f4a80a5704e1278f34eb1215
Ghaith Zamrik <Ghaith.Zamrik@akqa.com>
2017-04-27 15:14:25 +0200

    adding creating adn updating documents actions and reducers

--- src/reducers/documentReducer.js
+++ src/reducers/documentReducer.js
@@ -6,6 +6,19 @@ export default function documentReducer(state = initalState.documents, action) {
         case types.LOAD_DOCUMETNS_SUCCESS:
             return action.documents;
 
+        case types.CREATE_DOCUMENT_SUCCESS:
+            return [
+                ...state,
+                Object.assign({}, action.document)
+            ];
+
+        case types.UPDATE_DOCUMENT_SUCCESS:
+            return [
+                ...state.filter(document => document.id !== action.document.id),
+                Object.assign({}, action.document)
+            ];
+
+
     default:
         return state;
     }
